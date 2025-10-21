085f07e127d537bb8cd0dc56cf44f7343d31137a
Tqgeng <pavel.kramarew@mail.ru>
2025-10-21 21:31:16 +0300

    add item admin

---
+++ todo_manager/todo_list/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from todo_list.models import TodoItem
+
+
+@admin.register(TodoItem)
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "complete",
+    )
+    list_display_links = (
+        "id",
+        "title",
+    )
