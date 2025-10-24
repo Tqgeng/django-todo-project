eb96f9a67a9bc11d6eecbc4ad129446389651326
Tqgeng <pavel.kramarew@mail.ru>
2025-10-24 19:26:17 +0300

    add create path

--- todo_manager/todo_list/urls.py
+++ todo_manager/todo_list/urls.py
@@ -6,6 +6,7 @@ app_name = "todo_list"
 urlpatterns = [
     path("", views.TodoListIndexView.as_view(), name="index"),
     path("<int:pk>/", views.TodoDetailView.as_view(), name="detail"),
-    path("/list", views.TodoListView.as_view(), name="list"),
-    path("/complete", views.TodoListCompleteView.as_view(), name="complete"),
+    path("list/", views.TodoListView.as_view(), name="list"),
+    path("complete/", views.TodoListCompleteView.as_view(), name="complete"),
+    path("create/", views.TodoItemCreateView.as_view(), name="create"),
 ]
